6936b3b63a1aec08508d08ef1e8c674c2c835722
Eric Hocking <ehock7229@outlook.com>
2026-01-17 13:11:47 -0500

    Load CentralAuth if not on ldapwiki

--- GlobalSettings.php
+++ GlobalSettings.php
@@ -10,3 +10,27 @@ $wgHooks['BeforePageDisplay'][] = static function ( &$out, &$skin ) {
 	}
 	return true;
 };
+
+// Extensions
+if ( $wi->dbname !== 'ldapwikiwiki' ) {
+	wfLoadExtensions([
+		'CentralAuth',
+		'GlobalPreferences',
+		'GlobalBlocking',
+		'RemovePII',
+	]);
+
+	// Only allow users with global accounts to login
+	$wgCentralAuthStrict = true;
+	$wgCentralAuthEnableSul3 = false;
+
+	$wgCentralAuthAutoLoginWikis = $wmgCentralAuthAutoLoginWikis;
+
+	if ( isset( $wgAuthManagerAutoConfig['primaryauth'][LocalPasswordPrimaryAuthenticationProvider::class] ) ) {
+		$wgAuthManagerAutoConfig['primaryauth'][LocalPasswordPrimaryAuthenticationProvider::class]['args'][0]['loginOnly'] = true;
+	}
+
+	$wgPasswordConfig['null'] = [ 'class' => InvalidPassword::class ];
+
+	$wgLoginNotifyUseCentralId = true;
+}
